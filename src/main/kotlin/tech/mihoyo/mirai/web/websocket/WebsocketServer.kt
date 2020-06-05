2b97bbf39bca95e211a70e0549c0962df0d35cc5
yyuueexxiinngg <yyuueexxiinngg@outlook.com>
2020-06-05 10:33:15 +0800

    WIP: Add websocket server support

--- src/main/kotlin/tech/mihoyo/mirai/web/websocket/WebsocketServer.kt
+++ src/main/kotlin/tech/mihoyo/mirai/web/websocket/WebsocketServer.kt
@@ -1,2 +1,97 @@
 package tech.mihoyo.mirai.web.websocket
 
+import io.ktor.application.Application
+import io.ktor.application.install
+import io.ktor.routing.Route
+import io.ktor.routing.routing
+import io.ktor.server.cio.CIO
+import io.ktor.server.engine.ApplicationEngine
+import io.ktor.server.engine.applicationEngineEnvironment
+import io.ktor.server.engine.connector
+import io.ktor.server.engine.embeddedServer
+import io.ktor.util.KtorExperimentalAPI
+import io.ktor.util.pipeline.ContextDsl
+import io.ktor.websocket.DefaultWebSocketServerSession
+import io.ktor.websocket.WebSockets
+import io.ktor.websocket.webSocket
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import net.mamoe.mirai.console.plugins.PluginBase
+import net.mamoe.mirai.event.Listener
+import net.mamoe.mirai.event.events.BotEvent
+import net.mamoe.mirai.event.subscribeAlways
+import org.slf4j.helpers.NOPLoggerFactory
+import tech.mihoyo.mirai.util.logger
+import tech.mihoyo.mirai.web.BotSession
+import tech.mihoyo.mirai.web.HttpApiService
+import kotlin.coroutines.CoroutineContext
+
+
+
+
+class WebSocketServer(
+    override val console: PluginBase
+) : HttpApiService, CoroutineScope {
+    override val coroutineContext: CoroutineContext =
+        CoroutineExceptionHandler { _, throwable -> logger.error(throwable) }
+
+    lateinit var server: ApplicationEngine
+    private val configByBots: MutableMap<Long, WebSocketServerConfig> = mutableMapOf()
+    private var subscription: Listener<BotEvent>? = null
+
+    override fun onLoad() {
+    }
+
+    @KtorExperimentalAPI
+    override fun onEnable() {
+        subscription = console.subscribeAlways {
+            if (!configByBots.containsKey(bot.id)) {
+                val config = WebSocketServerConfig(console.loadConfig("setting.yml"), bot)
+                configByBots[bot.id] = config
+                logger.info("Bot:${bot.id} Websocket服务端模块启用状态: ${config.enable}")
+                if (config.enable) {
+                    launch {
+                        try {
+                            server = embeddedServer(CIO, environment = applicationEngineEnvironment {
+                                this.parentCoroutineContext = coroutineContext
+                                this.module(Application::cqWebsocketServer)
+
+
+                                connector {
+                                    this.host = host
+                                    this.port = port
+                                }
+                            })
+                        } catch (e: Exception) {
+                            logger.error("Bot:${bot.id} Websocket服务端模块启用失败")
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onDisable() {
+    }
+
+
+}
+
+fun Application.cqWebsocketServer() {
+
+    install(WebSockets)
+    routing {
+        webSocket("/event") {
+
+        }
+        webSocket("/api") {
+
+        }
+        webSocket("/") {
+
+        }
+    }
+}
+
+
--- src/main/kotlin/tech/mihoyo/mirai/web/websocket/WebsocketServerConfig.kt
+++ src/main/kotlin/tech/mihoyo/mirai/web/websocket/WebsocketServerConfig.kt
@@ -1,2 +1,17 @@
 package tech.mihoyo.mirai.web.websocket
 
+import net.mamoe.mirai.Bot
+import net.mamoe.mirai.console.plugins.Config
+
+class WebSocketServerConfig(config: Config, bot: Bot) {
+    @Suppress("UNCHECKED_CAST")
+    private val  botConfig = config[bot.id.toString()] as? Map<String, Any> ?: emptyMap()
+
+    @Suppress("UNCHECKED_CAST")
+    private val serviceConfig = botConfig["ws"] as? Map<String, Any> ?: emptyMap()
+    val enable: Boolean by serviceConfig.withDefault { false }
+    val postMessageFormat: String by serviceConfig.withDefault { "string" }
+    val wsHost: String by serviceConfig.withDefault { "0.0.0.0" }
+    val wsPort: String by serviceConfig.withDefault { 6700 }
+    val accessToken: String? by serviceConfig.withDefault { null }
+}
